4ae66607fe2e7d08052a21337d4b7749ac754ddb
Esdras Pinheiro <esdras.dev@hotmail.com>
2024-10-04 12:21:40 -0300

    create new class UsuarioEntity

---
+++ src/main/java/com/esdrasdev/cadastro_usuarios/infrastructure/entities/UsuarioEntity.java
@@ -0,0 +1,55 @@
+package com.esdrasdev.cadastro_usuarios.infrastructure.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "usuario")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UsuarioEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome", nullable = false)
+    private String nome;
+
+    @Column(name = "email", unique = true, nullable = false)
+    private String email;
+
+    @Column(name = "documento", nullable = false)
+    private String documento;
+
+    @Column(name = "data_cadastro")
+    private LocalDateTime dataCadastro;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "endereco_id", referencedColumnName = "id")
+    private EnderecoEntity endereco;
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UsuarioEntity that = (UsuarioEntity) o;
+        return Objects.equals(id, that.id) &&
+                Objects.equals(nome, that.nome) &&
+                Objects.equals(email, that.email) &&
+                Objects.equals(documento, that.documento) &&
+                Objects.equals(dataCadastro, that.dataCadastro) &&
+                Objects.equals(endereco, that.endereco);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, nome, email, documento, dataCadastro, endereco);
+    }
+}
