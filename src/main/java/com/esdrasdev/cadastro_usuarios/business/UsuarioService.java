4712605be0fc106f7c7474b48cd71d5d4e43a73f
Esdras Pinheiro <esdras.dev@hotmail.com>
2024-10-07 08:11:35 -0300

    Create UsuÃarioService

---
+++ src/main/java/com/esdrasdev/cadastro_usuarios/business/UsuarioService.java
@@ -0,0 +1,68 @@
+package com.esdrasdev.cadastro_usuarios.business;
+
+import com.esdrasdev.cadastro_usuarios.infrastructure.entities.UsuarioEntity;
+import com.esdrasdev.cadastro_usuarios.infrastructure.repository.UsuarioRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UsuarioService {
+
+    private final UsuarioRepository usuarioRepository;
+    private final UsuarioConverter usuarioConverter;
+    private final UsuarioUpdateMapper usuarioUpdateMapper;
+    private final UsuarioMapper usuarioMapper;
+
+
+    public UsuarioEntity salvaUsuario(UsuarioEntity usuarioEntity) {
+        return usuarioRepository.saveAndFlush(usuarioEntity);
+    }
+
+    public UsuarioResponseDTO gravarUsuarios(UsuarioRequestDTO usuarioRequestDTO) {
+        try {
+            notNull(usuarioRequestDTO, "Os dados do usuário são obrigatórios");
+            UsuarioEntity usuarioEntity = salvaUsuario(usuarioConverter.paraUsuarioEntity(usuarioRequestDTO));
+            return usuarioMapper.paraUsuarioResponseDTO(usuarioEntity);
+        } catch (Exception e) {
+            throw new BusinessException("Erro ao gravar dados de usuário", e);
+        }
+    }
+
+    public UsuarioResponseDTO atualizaCadastro(UsuarioRequestDTO usuarioRequestDTO) {
+        try {
+            notNull(usuarioRequestDTO, "Os dados do usuário são obrigatórios");
+            UsuarioEntity usuario = usuarioRepository.findByEmail(usuarioRequestDTO.getEmail());
+            usuarioUpdateMapper.updateUsuarioFromDTO(usuarioRequestDTO, usuario);
+            return usuarioMapper.paraUsuarioResponseDTO(salvaUsuario(usuario));
+        } catch (Exception e) {
+            throw new BusinessException("Erro ao gravar dados de usuário", e);
+        }
+    }
+
+    public UsuarioResponseDTO buscaDadosUsuario(String email) {
+        UsuarioEntity entity = usuarioRepository.selectFromEmail(email);
+
+        return entity != null ? usuarioMapper.paraUsuarioResponseDTO(entity) : null;
+    }
+
+    public List<UsuarioResponseDTO> buscaPorIniciais(String iniciais){
+        List<UsuarioEntity> entity = usuarioRepository.findByNomeOrEmailStartingWith(iniciais);
+
+        return entity.isEmpty() ? Collections.emptyList() : usuarioMapper.paraListaUsuarioResponseDTO(entity);
+    }
+
+    public void fazUpdateDeEmail(String email, String documento) {
+        usuarioRepository.updateEmail(documento, email);
+    }
+
+    public void deletaDadosUsuario(String email) {
+
+        usuarioRepository.deleteByEmail(email);
+
+    }
+
+}
