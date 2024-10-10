00396eb64a070f672fc21e6a71d5ecce61fe06cd
Esdras Pinheiro <esdras.dev@hotmail.com>
2024-10-10 09:25:41 -0300

    Create endpoints new class UsuarioController

---
+++ src/main/java/com/esdrasdev/cadastro_usuarios/api/UsuarioController.java
@@ -0,0 +1,51 @@
+package com.esdrasdev.cadastro_usuarios.api;
+
+import com.esdrasdev.cadastro_usuarios.api.request.UsuarioRequestDTO;
+import com.esdrasdev.cadastro_usuarios.business.UsuarioService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/user")
+@RequiredArgsConstructor
+public class UsuarioController {
+
+    private final UsuarioService usuarioService;
+
+    @PostMapping()
+    public ResponseEntity<UsuarioResponseDTO> gravaDadosUsuario(@RequestBody UsuarioRequestDTO usuarioRequestDTO) {
+        return ResponseEntity.ok(usuarioService.gravarUsuarios(usuarioRequestDTO));
+    }
+
+    @PutMapping()
+    public ResponseEntity<UsuarioResponseDTO> atualizaDadosUsuario(@RequestBody UsuarioRequestDTO usuarioRequestDTO) {
+        return ResponseEntity.ok(usuarioService.atualizaCadastro(usuarioRequestDTO));
+    }
+
+    @GetMapping()
+    public ResponseEntity<UsuarioResponseDTO> buscaUsuarioPorEmail(@RequestParam ("email") String email) {
+        return ResponseEntity.ok(usuarioService.buscaDadosUsuario(email));
+    }
+
+    @GetMapping("/iniciais")
+    public ResponseEntity<List<UsuarioResponseDTO>> buscaUsuarioPorIniciais(@RequestParam ("iniciais") String iniciais) {
+        return ResponseEntity.ok(usuarioService.buscaPorIniciais(iniciais));
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deletaDadosUsuario(@RequestParam ("email") String email) {
+        usuarioService.deletaDadosUsuario(email);
+        return ResponseEntity.accepted().build();
+    }
+
+    @PatchMapping
+    public ResponseEntity<Void> updateDeEmail(@RequestParam ("email") String email,
+                                              @RequestParam ("documento") String documento) {
+        usuarioService.fazUpdateDeEmail(email, documento);
+        return ResponseEntity.accepted().build();
+    }
+
+
+}
